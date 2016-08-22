8092269332f5d904d8a9e0b5632abc1d345ddfe1
Lucas Daiki <lucas.daiki@vizir.com.br>
2016-08-21 23:04:47 -0300

    implementing react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { Router, Route, Link, browserHistory } from 'react-router';
+
 import HelloWorld from './HelloWorld';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HelloWorld />
-      </div>
+      <Router history={browserHistory}>
+        <Route path="/" component={HelloWorld}>
+          <Route path="HelloWorld" component={HelloWorld}/>
+        </Route>
+      </Router>
     );
   }
 }

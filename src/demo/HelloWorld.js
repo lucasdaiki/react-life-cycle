5048e13f92c94f63f0ea9151c6ed7a08e60d3652
Lucas Daiki <lucas.daiki@vizir.com.br>
2016-08-22 00:09:57 -0300

    implementing some lifecycles manipulators

--- src/demo/HelloWorld.js
+++ src/demo/HelloWorld.js
@@ -2,39 +2,35 @@ import React, { Component } from 'react';
 import Nav from './Nav.js';
 
 export default class HelloWorld extends Component {
+  constructor(props){
+    super(props);
+    this.state = {count: -1};
+  }
+
   componentWillMount() {
-    console.log('HelloWorld', 'componentWillMount');
+    console.log('HelloWorld', '(0) componentWillMount');
   }
 
   componentDidMount() {
-    console.log('HelloWorld', 'componentDidMount');
-  }
-
-  componentWillReceiveProps() {
-    console.log('HelloWorld', 'componentWillReceiveProps');
-  }
-
-  shouldComponentUpdate() {
-    console.log('HelloWorld', 'shouldComponentUpdate');
-    return true;
-  }
-
-  componentWillUpdate() {
-    console.log('HelloWorld', 'componentWillUpdate');
-  }
-
-  componentDidUpdate() {
-    console.log('HelloWorld', 'componentDidUpdate');
+    console.log('HelloWorld', '(2) componentDidMount');
+    window.addEventListener('click', this.incrementCount);
   }
 
   componentWillUnmount() {
-    console.log('HelloWorld', 'componentWillUnmount');
+    console.log('HelloWorld', '(last) componentWillUnmount');
+    window.removeEventListener('click', this.incrementCount);
+  }
+
+  incrementCount = () => {
+    const {count} = this.state;
+    this.setState({count: count + 1});
   }
 
   render() {
-    console.log('HelloWorld', 'render');
+    console.log('HelloWorld', '(1) render');
+    console.log(this.state.count);
     return (
-      <Nav selectedItem="HelloWorld" />
+      <Nav selectedItem="HelloWorld" count={this.state.count} />
     );
   }
 }
--- src/demo/Nav.js
+++ src/demo/Nav.js
@@ -2,19 +2,35 @@ import React, { Component, PropTypes } from 'react';
 import {Link} from 'react-router';
 
 export default class ByeWorld extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: props.count
+    }
+  }
+
   static propTypes = {
-    selectedItem: PropTypes.string.isRequired
+    selectedItem: PropTypes.string.isRequired,
+    count: PropTypes.number
   };
 
-  redirectTo(path){
-    <Link to={`/users/${user.id}`} activeClassName="current">{user.name}</Link>
+  static defaultProps = {
+    count: 0
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.count !== undefined && nextProps.count % 2)
+      this.setState({count: nextProps.count});
   }
 
   render() {
     const {selectedItem} = this.props;
+    const {count} = this.state;
 
     return (
       <div>
+        <h1>{selectedItem}</h1>
+        <h2>Counter: {count}</h2>
         <ul>
           <li>
             <Link to="HelloWorld">Hello World</Link>
@@ -23,7 +39,6 @@ export default class ByeWorld extends Component {
             <Link to="ByeWorld">Bye World</Link>
           </li>
         </ul>
-        <h1>{selectedItem}</h1>
       </div>
     );
   }

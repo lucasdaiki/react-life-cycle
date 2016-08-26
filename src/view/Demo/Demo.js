a8ee6d6bbc9b3001612957e848acf833adc4229d
Lucas Daiki <lucas.daiki@vizir.com.br>
2016-08-26 00:08:26 -0300

    Implementing input

--- src/view/Demo/Demo.js
+++ src/view/Demo/Demo.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Input from '../../components/Input/Input.js';
 
 export default class Demo extends Component {
   render() {
     return (
-      <span>Olá</span>
+      <Input label="Name" />
     );
   }
 }

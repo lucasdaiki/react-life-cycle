b910caf214ed842cc8ef0552beab793ddefe2033
Lucas Daiki <lucas.daiki@sprinklr.com>
2016-08-26 18:09:13 -0300

    Add route to home

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Router, Route, browserHistory } from 'react-router';
 
 import Demo from '../view/Demo/Demo.js';
+import Home from '../view/Home/Home.js';
 import Main from '../view/Main/Main.js';
 
 function Routes() {
   return (
     <Router history={browserHistory}>
       <Route path="/" component={Main}>
+        <Route path="home" component={Home} />
         <Route path="demo" component={Demo} />
       </Route>
     </Router>

a106fd7b7b78800fb941d87870317116d4ca821f
Lucas Daiki <lucas.daiki@sprinklr.com>
2016-08-26 18:56:14 -0300

    Improvements demo form

--- src/view/Demo/Demo.js
+++ src/view/Demo/Demo.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 import Input from '../../components/Input/Input.js';
+import Button from '../../components/Button/Button.js';
 
 export default class Demo extends Component {
+  constructor(props){
+    super(props);
+    this.form = {};
+  }
+
+  save = () => {
+    console.log(this);
+  }
+
+  handleInputChange = (field, value) => {
+    this.form[field] = value;
+  }
+
   render() {
     return (
-      <Input label="Name" />
+      <div>
+        <Input label="Name" onChange={this.handleInputChange.bind(this, 'name')} />
+        <Input label="Age" type="number" onChange={this.handleInputChange.bind(this, 'age')} />
+        <Button onClick={this.save}>Save it!</Button>
+      </div>
     );
   }
 }

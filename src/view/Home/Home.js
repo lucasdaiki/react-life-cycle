3fbb94940c1cce8744a8ecba3adb02c938146c8f
Lucas Daiki <lucas.daiki@sprinklr.com>
2016-08-26 18:08:59 -0300

    Create home view

---
+++ src/view/Home/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Input from '../../components/Input/Input.js';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
